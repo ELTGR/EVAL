ab88bb0a3cc46473b328fc88c990d6116f9b2308
mialon <mialonbastien@gmail.com>
2025-03-19 16:40:49 +0100

    frontend

---
+++ pages/win.php
@@ -0,0 +1,23 @@
+<div class="game">
+    <main>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <h1 class="win">YOU WIN !</h1>
+    </main>
+</div>
