dd3a368b18b7ec08ff7b39893ad92002f397349f
ELTGR <eliott.deleplanque.pro@gmail.com>
2025-03-19 16:42:38 +0100

    aze

--- pages/login.php
+++ pages/login.php
@@ -10,7 +10,10 @@
         <form class="inputs" action="index.php" method="post">
             <input class="my_button" type="text" name="prenom_inscription" placeholder="Prenom" required>
             <input class="my_button" type="text" name="nom_inscription" placeholder="Nom" required>
-            <input class="my_button" type="text" name="genre_inscription" placeholder="Homme/Femme" required>
+            <select class="my_button" name="genre_inscription" size="1">
+                <option value="Homme">Homme
+                <option value="Femmme"> Femmme
+            </select>
             <input class="my_button" type="email" name="email_inscription" placeholder="Email" required>
             <input class="my_button" type="password" name="password_inscription" placeholder="Mot de passe" required>
             <button type="my_button" name ="inscription"  value ="1">S'inscrire</button>
